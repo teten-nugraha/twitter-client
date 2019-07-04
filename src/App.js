33ab4329b258b532e439cfc0a0528aa1c2860142
teten777 <teten.nugraha18@gmail.com>
2019-07-04 14:49:25 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//auth
+import jwt_decode from 'jwt-decode'
+import SecuredRoute from './util/SecuredRoute'
+
+// redux
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistor, store } from './store';
+
 
 import Header from './component/partials/Header';
-import Signin from './component/pages/auth/Signin.jsx'
-import SignUp from './component/pages/auth/SignUp.jsx'
-import ForgotPassword from './component/pages/auth/ForgotPassword'
-
-import Home from './component/pages/home/Home'
+import Routes from './Routes'
 
 function App() {
   return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <div>
-    
+      <Router>
       <Header />
 
       <div className="container-fluid gedf-wrapper" style={{ paddingTop:"80px" }}>
 
-        <Home />
+        <Routes />
 
       </div>
-
+      </Router>
     </div>
-
+    </PersistGate>
+    </Provider>
   );
 }
 
---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Route, Switch } from "react-router-dom";
+import SecuredRoute from './util/SecuredRoute'
+
+import Signin from './component/pages/auth/Signin.jsx'
+import SignUp from './component/pages/auth/SignUp.jsx'
+import ForgotPassword from './component/pages/auth/ForgotPassword'
+
+import Home from './component/pages/home/Home'
+
+class Routes extends Component {
+    render() {
+        return (
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Signin} />
+            </div>
+        )
+    }
+}
+
+export default Routes;
