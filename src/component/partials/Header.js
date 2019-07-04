916f076dcc93d3ffe069afac1f0425c431bbb7eb
teten777 <teten.nugraha18@gmail.com>
2019-07-04 15:14:00 +0700

    link logout

--- src/component/partials/Header.js
+++ src/component/partials/Header.js
@@ -18,18 +18,12 @@ class Header extends Component {
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">Profile</a>
                         </li>
-                        <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div className="dropdown-menu" aria-labelledby="dropdown01">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
+                    </ul>
+                    <ul className="navbar-nav px-3">
+                        <li className="nav-item text-nowrap">
+                        <a className="nav-link" href="#">Sign out</a>
                         </li>
                     </ul>
                     </div>
