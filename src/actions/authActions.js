b594d880a2fe4aa76186a21390be48f1687d8331
teten777 <teten.nugraha18@gmail.com>
2019-07-04 14:26:28 +0700

    authaction and authreducers

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -4,9 +4,40 @@ import {
 }from './types'
 import jwt_decode from 'jwt-decode'
 import axios from axios
+import {
+    API_URL
+}from '../util/Url'
 
 export const loginUser = userData => dispatch => {
 
-    axios.post("localhost:8080/api/users/login")
+    axios.post(API_URL + "/users/login", userData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
 
+        const { token } = response.data.data
+
+        //set token to ls
+        localStorage.setItem('jwtToken', token)
+
+        const decoded = jwt_decode(token)
+
+        dispatch(setCurrentUser(decoded)
+
+    }).catch(function(error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response
+        })
+    })
+
+}
+
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
 }
