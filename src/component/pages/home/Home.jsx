64a6958df8fcc42ffa3170930bd385add2bd2293
teten777 <teten.nugraha18@gmail.com>
2019-07-09 11:45:59 +0700

    update list tweets

--- src/component/pages/home/Home.jsx
+++ src/component/pages/home/Home.jsx
@@ -3,17 +3,25 @@ import axios from 'axios'
 import {
     API_URL
 }from '../../../util/Url'
+import { Instagram } from 'react-content-loader'
+import Tweet from './Tweet'
 
 class Home extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            newTweet: ""
+            newTweet: "",
+            tweets:[],
+            loadTweet:false
         }
         this.handleNewTweet = this.handleNewTweet.bind(this);
         
         
+    }
+
+    componentDidMount() {
+        this.loadTweets();
     }
 
     handleChange = event => {
@@ -44,8 +52,9 @@ class Home extends Component {
             this.setState({
                 newTweet: " "
             })
-
-            React.findDOMNode(this.refs.newTweet).value = "";
+            
+            this.loadTweets();
+           
             
         })
         .catch(function(error) {
@@ -57,8 +66,56 @@ class Home extends Component {
 
     }
 
+    loadTweets() {
+
+        this.setState({
+            loadTweet: true
+        })
+
+        axios.get(API_URL + "/tweets/home", {
+            headers: {
+                'Authorization': 'Bearer '+localStorage.getItem('jwtToken'),
+                'Content-type': 'application/json'
+            }
+        })
+        .then(response => {
+            
+            this.setState({
+                tweets: response.data.data,
+                loadTweet: false
+            })
+
+        })
+        .catch(function(error) {
+    
+            console.log(error)
+        })
+
+    }
+
+    populateTweets = () => {
+
+        let listTweet = null;
+        if(this.state.loadTweet) {
+            listTweet = <Instagram />
+        }else{
+            listTweet = this.state.tweets.map(tweet => {
+                return <Tweet 
+                    key={tweet.id}
+                    tweet={tweet.tweet}
+                    tweetMaker={tweet.user.fullname}
+                    created={tweet.create_At}
+                    />;
+            });
+        }
+
+        return listTweet;
+    }
+
 
     render() {
+
+
         return (
             <div className="row">
 
@@ -130,176 +187,9 @@ class Home extends Component {
                         </div>
                     </div>
 
-                    <div className="card gedf-card">
-                        <div className="card-header">
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="mr-2">
-                                        <img className="rounded-circle" width="45"
-                                            src="#"
-                                            alt="" />
-                                    </div>
-                                    <div className="ml-2">
-                                        <div className="h5 m-0">@LeeCross</div>
-                                        <div className="h7 text-muted">Miracles Lee Cross</div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="dropdown">
-                                        <button className="btn btn-link dropdown-toggle" type="button" id="gedf-drop1"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i className="fa fa-ellipsis-h"></i>
-                                        </button>
-                                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
-                                            <div className="h6 dropdown-header">Configuration</div>
-                                            <a className="dropdown-item" href="#">Save</a>
-                                            <a className="dropdown-item" href="#">Hide</a>
-                                            <a className="dropdown-item" href="#">Report</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <br />
 
-                        </div>
-                        <div className="card-body">
-                            <div className="text-muted h7 mb-2"> <i className="fa fa-clock-o"></i>10 min ago</div>
-                            <a className="card-link" href="#">
-                                <h5 className="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
-                            </a>
-
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa
-                                praesentium esse magnam nemo dolor
-                                sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-                            </p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="card-link"><i className="fa fa-gittip"></i> Like</a>
-                            <a href="#" className="card-link"><i className="fa fa-comment"></i> Comment</a>
-                            <a href="#" className="card-link"><i className="fa fa-mail-forward"></i> Share</a>
-                        </div>
-                    </div>
-
-                    <div className="card gedf-card">
-                        <div className="card-header">
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="mr-2">
-                                        <img className="rounded-circle" width="45"
-                                            src=""
-                                            alt="" />
-                                    </div>
-                                    <div className="ml-2">
-                                        <div className="h5 m-0">@LeeCross</div>
-                                        <div className="h7 text-muted">Miracles Lee Cross</div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="dropdown">
-                                        <button className="btn btn-link dropdown-toggle" type="button" id="gedf-drop1"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i className="fa fa-ellipsis-h"></i>
-                                        </button>
-                                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
-                                            <div className="h6 dropdown-header">Configuration</div>
-                                            <a className="dropdown-item" href="#">Save</a>
-                                            <a className="dropdown-item" href="#">Hide</a>
-                                            <a className="dropdown-item" href="#">Report</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="card-body">
-                            <div className="text-muted h7 mb-2"> <i className="fa fa-clock-o"></i> 10 min ago</div>
-                            <a className="card-link" href="#">
-                                <h5 className="card-title"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-                                    consectetur
-                                    deserunt illo esse distinctio.</h5>
-                            </a>
-
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis nihil, aliquam est,
-                                voluptates officiis iure soluta
-                                alias vel odit, placeat reiciendis ut libero! Quas aliquid natus cumque quae repellendus.
-                                Lorem
-                                ipsum dolor sit amet consectetur adipisicing elit. Ipsa, excepturi. Doloremque,
-                                reprehenderit!
-                                Quos in maiores, soluta doloremque molestiae reiciendis libero expedita assumenda fuga quae.
-                                Consectetur id molestias itaque facere? Hic!
-                            </p>
-                            <div>
-                                <span className="badge badge-primary">JavaScript</span>
-                                <span className="badge badge-primary">Android</span>
-                                <span className="badge badge-primary">PHP</span>
-                                <span className="badge badge-primary">Node.js</span>
-                                <span className="badge badge-primary">Ruby</span>
-                                <span className="badge badge-primary">Paython</span>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="card-link"><i className="fa fa-gittip"></i> Like</a>
-                            <a href="#" className="card-link"><i className="fa fa-comment"></i> Comment</a>
-                            <a href="#" className="card-link"><i className="fa fa-mail-forward"></i> Share</a>
-                        </div>
-                    </div>
-
-                    <div className="card gedf-card">
-                        <div className="card-header">
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="mr-2">
-                                        <img className="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="" />
-                                    </div>
-                                    <div className="ml-2">
-                                        <div className="h5 m-0">@LeeCross</div>
-                                        <div className="h7 text-muted">Miracles Lee Cross</div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="dropdown">
-                                        <button className="btn btn-link dropdown-toggle" type="button" id="gedf-drop1"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i className="fa fa-ellipsis-h"></i>
-                                        </button>
-                                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
-                                            <div className="h6 dropdown-header">Configuration</div>
-                                            <a className="dropdown-item" href="#">Save</a>
-                                            <a className="dropdown-item" href="#">Hide</a>
-                                            <a className="dropdown-item" href="#">Report</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="card-body">
-                            <div className="text-muted h7 mb-2"> <i className="fa fa-clock-o"></i> Hace 40 min</div>
-                            <a className="card-link" href="#">
-                                <h5 className="card-title">Totam non adipisci hic! Possimus ducimus amet, dolores illo ipsum
-                                    quos
-                                    cum.</h5>
-                            </a>
-
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sunt fugit reprehenderit
-                                consectetur exercitationem odio,
-                                quam nobis? Officiis, similique, harum voluptate, facilis voluptas pariatur dolorum tempora
-                                sapiente
-                                eius maxime quaerat.
-                                <a href="https://mega.nz/#!1J01nRIb!lMZ4B_DR2UWi9SRQK5TTzU1PmQpDtbZkMZjAIbv97hU"
-                                    target="_blank">https://mega.nz/#!1J01nRIb!lMZ4B_DR2UWi9SRQK5TTzU1PmQpDtbZkMZjAIbv97hU</a>
-                            </p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="card-link"><i className="fa fa-gittip"></i> Like</a>
-                            <a href="#" className="card-link"><i className="fa fa-comment"></i> Comment</a>
-                            <a href="#" className="card-link"><i className="fa fa-mail-forward"></i> Share</a>
-                        </div>
-                    </div>
-
-
+                    { this.populateTweets() }
 
                 </div>
 
---
+++ src/component/pages/home/Tweet.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Avatar from 'react-avatar';
+
+import TimeAgo from 'react-timeago'
+import idStrings from 'react-timeago/lib/language-strings/id'
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
+
+function Tweet(props) {
+
+    const formatter = buildFormatter(idStrings)
+
+    return (
+
+        <div className="card gedf-card " style={{ marginBottom:"20px" }}>
+            <div className="card-header">
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="mr-2">
+                        <Avatar color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} name={props.tweetMaker} size="40" />
+                        </div>
+                        <div className="ml-2">
+                            <div className="h5 m-0">@ {props.tweetMaker}</div>
+                            <div className="h7 text-muted">Miracles Lee Cross</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="dropdown">
+                            <button className="btn btn-link dropdown-toggle" type="button" id="gedf-drop1"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i className="fa fa-ellipsis-h"></i>
+                            </button>
+                            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
+                                <div className="h6 dropdown-header">Configuration</div>
+                                <a className="dropdown-item" href="#">Save</a>
+                                <a className="dropdown-item" href="#">Hide</a>
+                                <a className="dropdown-item" href="#">Report</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="card-body">
+                <div className="text-muted h7 mb-2"> <i className="fa fa-clock-o"></i>
+                    <TimeAgo date={props.created} formatter={formatter} />
+                </div>
+
+                <p className="card-text">
+                    {props.tweet}
+                </p>
+            </div>
+            <div className="card-footer">
+                <a href="#" className="card-link"><i className="fa fa-gittip"></i> Like</a>
+                <a href="#" className="card-link"><i className="fa fa-comment"></i> Comment</a>
+                <a href="#" className="card-link"><i className="fa fa-mail-forward"></i> Share</a>
+            </div>
+        </div>
+
+    );
+}
+
+export default Tweet;
